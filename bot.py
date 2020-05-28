b645cc9efdcb2363ccdbd25e5e811d47a47e9e9a
Kashish srivastava <44269697+cannibalcheeseburger@users.noreply.github.com>
2020-05-28 14:42:46 +0530

    added recipes

--- bot.py
+++ bot.py
@@ -2,9 +2,13 @@ import discord
 from discord.ext import commands
 import random
 import creds
+from const import responses
+import src
+
 
 client = commands.Bot(command_prefix='--')
 
+# EVENTS
 @client.event
 async def on_ready():
     await client.change_presence(status= discord.Status.online,activity=discord.Game('RaMu bhaiya Uth Gaye'))
@@ -18,42 +22,32 @@ async def on_member_join(member):
 async def on_member_remove(member):
     print(f'{member} Bhaiya Chale gye. Paise kon de raha hai fir.')
 
+
+# COMMANDS
+
+## PING
 @client.command(help="Returns Ping",description="Returns Ping")
 async def ping(ctx):
     await ctx.send(f'Pencho! {round(client.latency *1000)} ms')
 
+## 8 ball
 @client.command(aliases = ['8ball'],help="Return a response to the question",description="Return a response to the question")
 async def _8ball(ctx, * ,question):
-    responses = ["As I see it, yes.",
-                "Ask again later.",
-                "Better not tell you now.",
-                "Cannot predict now.",
-                "Concentrate and ask again.",
-                "Don’t count on it.",
-                "It is certain.",
-                "It is decidedly so.",
-                "Most likely.",
-                "My reply is no.",
-                "My sources say no.",
-                "Outlook not so good.",
-                "Outlook good.",
-                "Reply hazy, try again.",
-                "Signs point to yes.",
-                "Very doubtful.",
-                "Without a doubt.",
-                "Yes.",
-                "Yes – definitely.",
-                "You may rely on it.",
-                "Tu PENCHOD HAI AUR KUCH NI"
-                ]
     await ctx.send(f'Question: {question}\nAnswer:{random.choice(responses)}')
 
+##OOBHAI
 @client.command(help = "Returns maro mujhe maro x number of times",description="Returns maro mujhe maro x number of times")
-async def oobhai(ctx,no):
-    for i in range(int(no)):
-        await ctx.send("Maro mujhe maro")
-        if int(no) >10:
-            break;
+async def oobhai(ctx,no:int):
+    if no < 10:
+        await ctx.send("Maro mujhe maro"*no)
+    else:
+        await ctx.send("Maro mujhe maro"*10)
+    
+## RECIPE
+@client.command(help="Returns recipe of the day",description="Returns recipe of the day")
+async def recipe(ctx):
+    await ctx.send(src.recipe.recipeofday(creds.FOOD))
+
 
 @client.command(help ="Clears x number of messages",description="Clears x number of messages")
 async def clear(ctx,amount = 5):
---
+++ src/recipe.py
@@ -0,0 +1,54 @@
+import datetime
+import requests
+import json
+import re
+clean = re.compile('<.*?>')
+
+
+def recipeofday(token):
+    today_date = datetime.date.today()
+    reci = open('reci.txt','r+')
+
+
+    text = reci.read()
+
+    if not str(today_date) == text.partition('\n')[0].strip():
+        #truncate file
+        reci.seek(0)
+        reci.truncate(0)
+        
+        #get recipe and save in RECIPE
+        url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random"
+
+        querystring = {"number":"1","tags":"vegetarian%2Cdessert"}
+
+        headers = {
+            'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+            'x-rapidapi-key': "824bc0944dmsha1058bd1b0cc53ap1e9c21jsnb8d7f51fef9e"
+            }
+
+        response = requests.request("GET", url, headers=headers, params=querystring)
+
+        data  = response.json()
+        title = ""
+        ingre = ""
+        inst =""
+        for item  in data['recipes']:
+            title = item['title']
+            count = 1
+            for ing in item['extendedIngredients']:
+                ingre = ingre + str(count)+'. '+ing['name']+'\n'+ing['original']+'\n'
+                count = count +1
+            inst = re.sub(clean,'',item['instructions'])
+     
+        #add recipe to text
+        text = str(today_date).strip() + """\nRECIPE OF THE DAY\n""" + title+'\n\nINGREDIENTS\n'+ingre+"\n\nINSTRUCTIONS\n"+inst
+        
+        #replace text to file
+        reci.writelines(text)
+
+    reci.close()
+    #return this
+    return(text)
+
+
